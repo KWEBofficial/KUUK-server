66868612cc5d7a1cdd838d91fa50a6915ee70a6f
cympyc1785 <cympyc1785@gmail.com>
2024-01-03 23:17:54 +0900

    ADD, MOD: 임시 poll service 추가, dependency 해결을 위한 router, repository 수정

--- src/repository/db.repository.ts
+++ src/repository/db.repository.ts
@@ -1,8 +1,26 @@
 import AppDataSource from '../config/dataSource';
+import Candidate from '../entity/candidate.entity';
+import Menu from '../entity/menu.entity';
+import Participant from '../entity/participant.entity';
+import Poll from '../entity/poll.entity';
+import Restaurant from '../entity/restaurant.entity';
 import User from '../entity/user.entity';
+import Vote from '../entity/vote.entity';
 
-// 예시 repository입니다. 필요에 따라 수정하거나 삭제하셔도 됩니다.
-
+const CandidateRepository = AppDataSource.getRepository(Candidate).extend({});
+const MenuRepository = AppDataSource.getRepository(Menu).extend({});
+const ParticipantRepository = AppDataSource.getRepository(Participant).extend({});
+const PollRepository = AppDataSource.getRepository(Poll).extend({});
+const RestaurantRepository = AppDataSource.getRepository(Restaurant).extend({});
 const UserRepository = AppDataSource.getRepository(User).extend({});
+const VoteRepository = AppDataSource.getRepository(Vote).extend({});
 
-export {UserRepository};
+export {
+  CandidateRepository,
+  MenuRepository,
+  ParticipantRepository,
+  UserRepository,
+  PollRepository,
+  RestaurantRepository,
+  VoteRepository
+};
