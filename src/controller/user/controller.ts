208cd154206abc40892c0f237d04780bd044d872
JeongYeonSeung <asas4654@daum.net>
2024-01-05 14:56:18 +0900

    MOD : 세션 및 로그인/회원가입 기능 추가

--- src/controller/user/controller.ts
+++ src/controller/user/controller.ts
@@ -1,68 +1,60 @@
 import { RequestHandler } from 'express';
 import UserService from '../../service/user.service';
 import CreateUserInput from '../../type/user/create.input';
-import { BadRequestError } from '../../util/customErrors';
+import { BadRequestError, UnauthorizedError } from '../../util/customErrors';
 
-export const getUserById: RequestHandler = async (req, res, next) => {
+// POST /user/login
+export const loginUser: RequestHandler = async (req, res, next) => {
   try {
-    const id = Number(req.query.id);
-
-    const user = await UserService.getUserById(id);
-    if (!user) throw new BadRequestError('해당하는 유저가 없습니다.');
-
+    // body에서 username, password 받아오기
+    const { username, password } = req.body;
+    // 누락되었다면 BadRequestError (?) 
+    if (!username || !password) throw new BadRequestError("아이디와 비밀번호를 모두 입력해주세요");
+    // username에 해당하는 user 정보 가져오기
+    const user = await UserService.getUsersByUsername(username);
+    // 없다면 UnauthorizedError (?)
+    if (!user) throw new UnauthorizedError('해당하는 유저가 없습니다.');
+    // 비밀번호 검증 단계 필요? 해싱한다면 준스처럼 구현하면 될 것 같고
+    // 필요없다면 user의 password와 비교 (?)
+    if (password !== user[0].password) throw new UnauthorizedError('비밀번호가 옳지 않습니다.');
+    // 모두 통과했다면 user 정보를 session에 저장!
+    req.session.user = {
+      id: user[0].id,
+      username: user[0].username,
+      displayName: user[0].displayName,
+    }; // 정보 더 추가해야 하는가?
+    // 메인 페이지로 redirection 추가 필요
     res.json(user);
   } catch (error) {
     next(error);
   }
 };
 
-// Params 이용 예시
-// export const getUsersByAge: RequestHandler = async (req, res, next) => {
-//   try {
-//     const age = Number(req.params.age);
-
-//     const users = await UserService.getUsersByAge(age);
-
-//     res.json(users);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-export const getUsersByUsername: RequestHandler = async (req, res, next) => {
-  try {
-    const username = Number(req.query.username);
-
-    const users = await UserService.getUserById(username);
-    // 리스트 검사 필요?
-
-    res.json(users);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const getUsersByBirthdate: RequestHandler = async (req, res, next) => {
-  try {
-    const birthdate = new Date(Date.parse(String(req.query.birthdate))); // 임시 parsing
-    if (!birthdate) throw new BadRequestError('유효하지 않은 생년월일입니다.');
-
-    const users = await UserService.getUsersByBirthdate(birthdate);
-
-    res.json(users);
-  } catch (error) {
-    next(error);
-  }
-};
-
+// POST /user/join
 export const createUser: RequestHandler = async (req, res, next) => {
   try {
     const { username, displayName, password, birthdate } = req.body as CreateUserInput;
     const createUserInput: CreateUserInput = { username, displayName, password, birthdate };
-
+    // 중복 검사 (?)
+    const existingUser = await UserService.getUsersByUsername(username);
+    if (existingUser) throw new BadRequestError('이미 존재하는 아이디입니다.')
+    // 유저 정보 저장 (password 암호화 해줄 것인지?)
     const user = await UserService.saveUser(createUserInput);
-
+    // 로그인 페이지로 redirection 추가 필요
     res.status(201).json(user.id);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// POST /user/logout
+export const logoutUser: RequestHandler = async (req, res, next) => {
+  try {
+    // session destroy 해주기
+    req.session.destroy((err) => {
+      if (err) throw err;
+      else return; // 메인페이지로 redirect 추가 필요
+    })
   } catch (error) {
     next(error);
   }
