84a99139ce855d560b8f441400d2f0b056c1df5e
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 <sue.gus77@gmail.com>
2024-01-08 00:44:02 +0900

    MOD: login router 수정

--- src/controller/user/router.ts
+++ src/controller/user/router.ts
@@ -3,7 +3,7 @@ import { createUser, loginUser, logoutUser } from './controller';
 
 const userRouter = Router();
 
-userRouter.get('/login', loginUser); // query를 사용하여 id에 맞는 user 정보를 가져오는 api ex) GET http://localhost:3000/user?id=1
+userRouter.post('/login', loginUser); // query를 사용하여 id에 맞는 user 정보를 가져오는 api ex) GET http://localhost:3000/user?id=1
 // userRouter.get('/:age', getUsersByAge); // param을 사용하여 age에 맞는 user들 정보를 가져오는 api ex) GET http://localhost:3000/user/23
 userRouter.get('/', logoutUser);
 userRouter.post('/join', createUser); // body를 사용하여 user 정보를 저장하는 api ex) POST http://localhost:3000/user | body: { "firstName": "John", "lastName": "Doe", "age": 23 }
