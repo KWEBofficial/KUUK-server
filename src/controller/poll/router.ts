e9fed90ecbe8969cb6a9b827d523144f87973fd3
정혜민 <mini010523@gmail.com>
2024-01-09 04:05:35 +0900

    MOD: Poll router 수정, poll service에 필터링, 후보저장 기능 추가

--- src/controller/poll/router.ts
+++ src/controller/poll/router.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { getPollById, getPollsByPollName, createPoll} from './controller';
+import {
+  getSettingform,
+  createFilteredRestaurants,
+  creatPollAndCandidate,
+} from './controller';
 
 const pollRouter = Router();
 
-pollRouter.get('/', getPollById);
-pollRouter.get('/', getPollsByPollName);
-pollRouter.get('/', createPoll);
+// pollRouter.get('/', getPollById);
+
+// 두 라우터를 구분해야 함
+pollRouter.get('/', getSettingform);
+pollRouter.get('/restaurant', createFilteredRestaurants);
+pollRouter.post('/restaurant', creatPollAndCandidate);
 
 export default pollRouter;
--- src/service/poll.service.ts
+++ src/service/poll.service.ts
@@ -2,6 +2,13 @@ import Poll from '../entity/poll.entity';
 import CreatePollInput from '../type/poll/create.input';
 import PollRepository from '../repository/poll.repository';
 import { InternalServerError } from '../util/customErrors';
+import Restaurant from '../entity/restaurant.entity';
+import RestaurantRepository from '../repository/restaurant.repository';
+import { In } from 'typeorm';
+import Candidate from '../entity/candidate.entity';
+import CandidateRepository from '../repository/candidate.repository';
+import LocationRepository from '../repository/location.repository';
+import CategoryRepository from '../repository/category.repository';
 
 export default class PollService {
   static async getPollById(id: number): Promise<Poll | null> {
@@ -26,6 +33,73 @@ export default class PollService {
       return await PollRepository.save(userEntity);
     } catch (error) {
       throw new InternalServerError('투표 정보를 저장하는데 실패했습니다.');
+    }
+  }
+
+  static async getRestaurantsByFiltering(
+    locations: string[] | string,
+    categories: string[] | string,
+  ): Promise<Restaurant[]> {
+    try {
+      // locations와 categories가 단일 문자열이라면 배열 형태로 변환하여 find 수행
+      const locationsArray = Array.isArray(locations) ? locations : [locations];
+      const categoriesArray = Array.isArray(categories)
+        ? categories
+        : [categories];
+
+      return await RestaurantRepository.find({
+        where: {
+          location: { locationName: In(locationsArray) },
+          categories: { categoryName: In(categoriesArray) },
+        },
+      });
+    } catch (error) {
+      throw new InternalServerError('레스토랑 정보를 불러오는데 실패했습니다.');
+    }
+  }
+
+  static async createCandidate(
+    poll: Poll,
+    selectedRestaurants: Restaurant,
+  ): Promise<Candidate> {
+    try {
+      const candidateEntity = await CandidateRepository.create({
+        poll: poll,
+        restaurants: selectedRestaurants,
+      });
+      return await CandidateRepository.save(candidateEntity);
+    } catch (error) {
+      throw new InternalServerError('투표 정보를 저장하는데 실패했습니다.');
+    }
+  }
+
+  // location 테이블에서 전체 locationName을 추출하여 반환
+  static async getAllLocations(): Promise<string[]> {
+    try {
+      const locations = await LocationRepository.find({
+        select: ['locationName'],
+      });
+
+      // locations 배열에서 locationName 값만 추출하여 반환
+      return locations.map((location) => location.locationName);
+    } catch (error) {
+      console.error('Error fetching locations:', error);
+      throw error;
+    }
+  }
+
+  // category 테이블에서 전체 categoryName을 추출하여 반환
+  static async getAllCategories(): Promise<string[]> {
+    try {
+      const categories = await CategoryRepository.find({
+        select: ['categoryName'],
+      });
+
+      // locations 배열에서 locationName 값만 추출하여 반환
+      return categories.map((category) => category.categoryName);
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+      throw error;
     }
   }
 }
